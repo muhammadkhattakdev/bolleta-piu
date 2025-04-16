9d65906f6763495248ad9dd20378eefec7f197ad
Muhammad Ayaz <ayazkhattakupwork@gmail.com>
2025-04-16 21:41:23 +0500

    Added

--- api/server.js
+++ api/server.js
@@ -24,11 +24,11 @@ app.use(cors({
 }));
 
 
-app.use(express.static(path.join(__dirname, "/client/dist")))
+// app.use(express.static(path.join(__dirname, "/client/dist")))
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/client/dist/index.html"))
-})
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "/client/dist/index.html"))
+// })
 
 
 
