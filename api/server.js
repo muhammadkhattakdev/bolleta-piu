b0758578a5d67821c1c7d82c3c54ccb08c217a8e
Muhammad Ayaz <ayazkhattakupwork@gmail.com>
2025-04-16 23:05:58 +0500

    Added

--- api/server.js
+++ api/server.js
@@ -17,12 +17,30 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+
+
+
+const allowedOrigins = [
+  process.env.CLIENT_URL,
+  'https://bolleta-piu-basm.vercel.app',
+  'https://bolleta-piu.vercel.app'
+];
+
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: function(origin, callback) {
+    if(!origin) return callback(null, true);
+    
+    if(allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   credentials: true,
-  methods: ['GET', "PUT", "POST", "DELETE"]
+  methods: ['GET', 'PUT', 'POST', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.options('*', cors());
+
 
 const uploadsDir = path.join(__dirname, 'uploads');
 const documentsDir = path.join(uploadsDir, 'documents');
